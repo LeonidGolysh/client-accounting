766b62af619453c45a569f1109ac7fba7794a21d
Leonid <leonidg34@gmail.com>
2024-07-09 11:43:13 +0200

    ferst commit

---
+++ src/main/java/com/ua/client_accounting/ClientAccountingApplication.java
@@ -0,0 +1,13 @@
+package com.ua.client_accounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientAccountingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientAccountingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ua/client_accounting/ClientAccountingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ua.client_accounting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientAccountingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
