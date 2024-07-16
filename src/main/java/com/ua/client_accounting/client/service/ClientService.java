bd98c0842080cc395a4b71886e87596d61d67c0b
Leonid <leonidg34@gmail.com>
2024-07-16 12:18:57 +0200

    client controller "delete client"

--- src/main/java/com/ua/client_accounting/client/service/ClientService.java
+++ src/main/java/com/ua/client_accounting/client/service/ClientService.java
@@ -1,7 +1,15 @@
 package com.ua.client_accounting.client.service;
 
+import com.ua.client_accounting.client.dto.create.CreateClientRequest;
 import com.ua.client_accounting.client.dto.create.CreateClientResponse;
+import com.ua.client_accounting.client.entity.Client;
+
+import java.util.List;
+import java.util.UUID;
 
 public interface ClientService {
-    CreateClientResponse createClient(CreateClientResponse createClientResponse);
+    List<Client> getAllClients();
+    CreateClientResponse createClient(CreateClientRequest request);
+    Client getClientById(UUID id);
+    void deleteClient(UUID id);
 }
--- src/main/java/com/ua/client_accounting/client/service/ClientServiceImpl.java
+++ src/main/java/com/ua/client_accounting/client/service/ClientServiceImpl.java
@@ -4,20 +4,26 @@ import com.ua.client_accounting.client.dto.create.CreateClientRequest;
 import com.ua.client_accounting.client.dto.create.CreateClientResponse;
 import com.ua.client_accounting.client.entity.Client;
 import com.ua.client_accounting.client.repository.ClientRepository;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
-public class ClientServiceImpl {
+
+public class ClientServiceImpl implements ClientService{
     private final ClientRepository clientRepository;
 
+    @Override
     public List<Client> getAllClients() {
         return clientRepository.findAll();
     }
 
+    @Override
     public CreateClientResponse createClient(CreateClientRequest request) {
         Client client = new Client();
 
@@ -30,5 +36,16 @@ public class ClientServiceImpl {
         response.setClientId(client.getId());
 
         return response;
+    }
+
+    @Override
+    public Client getClientById(UUID id) {
+        return clientRepository.findById(id).orElseThrow(() -> new RuntimeException("Client not found"));
+    }
+
+    @Override
+    public void deleteClient(UUID id) {
+        Client client = getClientById(id);
+        clientRepository.delete(client);
     }
 }
