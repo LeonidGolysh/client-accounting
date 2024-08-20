1d4dfd087138973490caff8645f6727cb90f7e82
Leonid <leonidg34@gmail.com>
2024-08-20 11:33:20 +0200

    test car controller get car by id

--- src/test/java/com/ua/client_accounting/car/controller/CarControllerTest.java
+++ src/test/java/com/ua/client_accounting/car/controller/CarControllerTest.java
@@ -20,7 +20,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -59,7 +59,7 @@ class CarControllerTest {
 
         when(carService.getAllCars()).thenReturn(carList);
 
-        //Convert client to JSON
+        //Convert car to JSON
         String carJson = objectMapper.writeValueAsString(carList);
 
         // Act & Assert
@@ -67,5 +67,29 @@ class CarControllerTest {
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().json(carJson));
+
+        verify(carService, times(1)).getAllCars();
+    }
+
+    @Test
+    void getCarByIdTest() throws Exception {
+        //Arrange
+        Client client = new Client(clientId, "Bob", "1234567890");
+        Car car = new Car(carId, client, "BMW", "Red", "ABC123");
+
+        when(carService.getCarById(carId)).thenReturn(car);
+
+        //Convert car to JSON
+        String carJson = objectMapper.writeValueAsString(car);
+        System.out.println("Serialized car JSON: " + carJson);
+
+        //Act & Assert
+        mockMvc.perform(get("/api/V2/cars/" + carId.toString())
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(carJson))
+                .andReturn();
+
+        verify(carService, times(1)).getCarById(carId);
     }
 }
