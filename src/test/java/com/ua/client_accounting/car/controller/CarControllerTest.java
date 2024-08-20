82933ec8f836cfda2aed2f7b8d09979884dc7b54
Leonid <leonidg34@gmail.com>
2024-08-20 13:04:13 +0200

    test car controller create car

--- src/test/java/com/ua/client_accounting/car/controller/CarControllerTest.java
+++ src/test/java/com/ua/client_accounting/car/controller/CarControllerTest.java
@@ -1,6 +1,8 @@
 package com.ua.client_accounting.car.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ua.client_accounting.car.dto.create.CreateCarRequest;
+import com.ua.client_accounting.car.dto.create.CreateCarResponse;
 import com.ua.client_accounting.car.entity.Car;
 import com.ua.client_accounting.car.service.CarService;
 import com.ua.client_accounting.client.entity.Client;
@@ -22,8 +24,8 @@ import java.util.UUID;
 
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 class CarControllerTest {
 
@@ -81,7 +83,6 @@ class CarControllerTest {
 
         //Convert car to JSON
         String carJson = objectMapper.writeValueAsString(car);
-        System.out.println("Serialized car JSON: " + carJson);
 
         //Act & Assert
         mockMvc.perform(get("/api/V2/cars/" + carId.toString())
@@ -91,5 +92,32 @@ class CarControllerTest {
                 .andReturn();
 
         verify(carService, times(1)).getCarById(carId);
+    }
+
+    @Test
+    void createCarTest() throws Exception {
+        //Assert
+        CreateCarRequest request = new CreateCarRequest();
+        request.setClientId(clientId);
+        request.setCarModel("BMW");
+        request.setCarColor("Red");
+        request.setCarNumberPlate("ABC123");
+
+        CreateCarResponse response = new CreateCarResponse();
+        response.setCarId(carId);
+
+        when(carService.createCar(any(CreateCarRequest.class))).thenReturn(response);
+
+        //Convert request to JSON
+        String requestJson = objectMapper.writeValueAsString(request);
+
+        //Act & Assert
+        mockMvc.perform(post("/api/V2/cars/create")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestJson))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.carId").value(carId.toString()));
+
+        verify(carService, times(1)).createCar(any(CreateCarRequest.class));
     }
 }
