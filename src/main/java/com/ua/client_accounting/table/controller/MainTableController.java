7676c75c1b06e81ead8c2fccae1ec4d9b54b3594
Leonid <leonidg34@gmail.com>
2024-09-11 14:48:26 +0200

    refactored code

--- src/main/java/com/ua/client_accounting/table/controller/MainTableController.java
+++ src/main/java/com/ua/client_accounting/table/controller/MainTableController.java
@@ -1,8 +1,8 @@
-package com.ua.client_accounting.car.controller;
+package com.ua.client_accounting.table.controller;
 
-import com.ua.client_accounting.car.dto.ClientCarDTO;
+import com.ua.client_accounting.table.dto.MainTableDTO;
 import com.ua.client_accounting.car.entity.Car;
-import com.ua.client_accounting.car.service.ClientCarServiceImpl;
+import com.ua.client_accounting.table.service.MainTableService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,19 +14,19 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api/V2/client-car")
 @AllArgsConstructor
-public class ClientCarController {
+public class MainTableController {
 
-    private ClientCarServiceImpl clientCarService;
+    private MainTableService mainTableService;
 
     @GetMapping
-    public ResponseEntity<List<ClientCarDTO>> getClientCars() {
-        List<ClientCarDTO> clientCarDTOS = clientCarService.getClientCars();
+    public ResponseEntity<List<MainTableDTO>> getClientCars() {
+        List<MainTableDTO> clientCarDTOS = mainTableService.getClientCars();
         return ResponseEntity.ok(clientCarDTOS);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ClientCarDTO> getClientCarById(@PathVariable("id") UUID carId) {
-        ClientCarDTO clientCarDTO = clientCarService.getClientCarById(carId);
+    public ResponseEntity<MainTableDTO> getClientCarById(@PathVariable("id") UUID carId) {
+        MainTableDTO clientCarDTO = mainTableService.getClientCarById(carId);
         if (clientCarDTO != null) {
             return ResponseEntity.ok(clientCarDTO);
         } else {
@@ -35,8 +35,8 @@ public class ClientCarController {
     }
 
     @GetMapping("/model/{carModel}")
-    public ResponseEntity<List<ClientCarDTO>> getClientCarsByModel(@PathVariable("carModel") String carModel) {
-        List<ClientCarDTO> clientCarDTOs = clientCarService.getClientCarByModel(carModel);
+    public ResponseEntity<List<MainTableDTO>> getClientCarsByModel(@PathVariable("carModel") String carModel) {
+        List<MainTableDTO> clientCarDTOs = mainTableService.getClientCarByModel(carModel);
         if (!clientCarDTOs.isEmpty()) {
             return ResponseEntity.ok(clientCarDTOs);
         } else {
@@ -45,8 +45,8 @@ public class ClientCarController {
     }
 
     @PostMapping("/create")
-    public ResponseEntity<Car> createCarWithClient(@RequestBody ClientCarDTO clientCarDTO) {
-        Car car = clientCarService.createCarWithClient(clientCarDTO);
+    public ResponseEntity<Car> createCarWithClient(@RequestBody MainTableDTO mainTableDTO) {
+        Car car = mainTableService.createCarWithClient(mainTableDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(car);
     }
 }
---
+++ src/main/java/com/ua/client_accounting/table/service/MainTableService.java
@@ -0,0 +1,14 @@
+package com.ua.client_accounting.table.service;
+
+import com.ua.client_accounting.car.entity.Car;
+import com.ua.client_accounting.table.dto.MainTableDTO;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface MainTableService {
+    List<MainTableDTO> getClientCars();
+    MainTableDTO getClientCarById(UUID carId);
+    List<MainTableDTO> getClientCarByModel(String carModel);
+    Car createCarWithClient(MainTableDTO mainTableDTO);
+}
