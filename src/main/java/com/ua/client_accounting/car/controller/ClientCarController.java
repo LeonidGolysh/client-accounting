bee057983017c7a5485883403ebc7560126d54d7
Leonid <leonidg34@gmail.com>
2024-09-11 13:55:32 +0200

    client car get by id and by model

--- src/main/java/com/ua/client_accounting/car/controller/ClientCarController.java
+++ src/main/java/com/ua/client_accounting/car/controller/ClientCarController.java
@@ -5,21 +5,43 @@ import com.ua.client_accounting.car.service.ClientCarServiceImpl;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/V2")
+@RequestMapping("/api/V2/client-car")
 @AllArgsConstructor
 public class ClientCarController {
 
     private ClientCarServiceImpl clientCarService;
 
-    @GetMapping("/client-car")
+    @GetMapping
     public ResponseEntity<List<ClientCarDTO>> getClientCars() {
         List<ClientCarDTO> clientCarDTOS = clientCarService.getClientCars();
         return ResponseEntity.ok(clientCarDTOS);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClientCarDTO> getClientCarById(@PathVariable("id") UUID carId) {
+        ClientCarDTO clientCarDTO = clientCarService.getClientCarById(carId);
+        if (clientCarDTO != null) {
+            return ResponseEntity.ok(clientCarDTO);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/model/{carModel}")
+    public ResponseEntity<List<ClientCarDTO>> getClientCarsByModel(@PathVariable("carModel") String carModel) {
+        List<ClientCarDTO> clientCarDTOs = clientCarService.getClientCarByModel(carModel);
+        if (!clientCarDTOs.isEmpty()) {
+            return ResponseEntity.ok(clientCarDTOs);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
