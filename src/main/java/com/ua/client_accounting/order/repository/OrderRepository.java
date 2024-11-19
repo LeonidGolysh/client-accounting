61063bf5505978e5e1f13c66d8f193c07a93b8f2
Leonid <leonidg34@gmail.com>
2024-11-19 17:00:14 +0100

    rewrite update method in MainTableServiceImpl

--- src/main/java/com/ua/client_accounting/order/repository/OrderRepository.java
+++ src/main/java/com/ua/client_accounting/order/repository/OrderRepository.java
@@ -1,11 +1,15 @@
 package com.ua.client_accounting.order.repository;
 
+import com.ua.client_accounting.car.entity.Car;
 import com.ua.client_accounting.order.entity.Order;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, UUID> {
+    List<Order> findByCar(Car car);
 }
--- src/main/java/com/ua/client_accounting/table/service/MainTableService.java
+++ src/main/java/com/ua/client_accounting/table/service/MainTableService.java
@@ -1,6 +1,5 @@
 package com.ua.client_accounting.table.service;
 
-import com.ua.client_accounting.car.entity.Car;
 import com.ua.client_accounting.order.entity.Order;
 import com.ua.client_accounting.table.dto.MainTableDTO;
 
@@ -12,6 +11,6 @@ public interface MainTableService {
     MainTableDTO getClientCarById(UUID carId);
     List<MainTableDTO> getClientCarByModel(String carModel);
     Order createCarWithClient(MainTableDTO mainTableDTO);
-    Car updateCarWithClient(UUID carId, MainTableDTO mainTableDTO);
+    Order updateCarWithClient(UUID orderId, MainTableDTO mainTableDTO);
     void deleteCarAndClientIfNoMoreCars(UUID carId);
 }
