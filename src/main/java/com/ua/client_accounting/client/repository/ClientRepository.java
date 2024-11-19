f99f26cebbf22e8d017afe022c5cc9c78183de2c
Leonid <leonidg34@gmail.com>
2024-11-19 17:29:39 +0100

    rewrite delete method in MainTableServiceImpl

--- src/main/java/com/ua/client_accounting/client/repository/ClientRepository.java
+++ src/main/java/com/ua/client_accounting/client/repository/ClientRepository.java
@@ -4,8 +4,10 @@ import com.ua.client_accounting.client.entity.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, UUID> {
+    Optional<Client> findByNameAndPhoneNumber(String name, String phoneNumber);
 }
